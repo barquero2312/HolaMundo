5b337d26a063c52d414b32fc2571f7ef224ae254
barquero2312 <barquero2312@hotmail.com>
2022-11-07 10:43:19 -0600

    Cambios de prueba

--- backend/Prueba.js
+++ backend/Prueba.js
@@ -2,7 +2,7 @@ function PruebaNodejs(number){
 
     console.log('Hola mundo! Me has pasado por parametro el número ' + number);
 }
-
+//TEXTO DE PRUEBA 2
 function exec() {
     for (let index = 0; index < 100; index++) {
         PruebaNodejs(index);
